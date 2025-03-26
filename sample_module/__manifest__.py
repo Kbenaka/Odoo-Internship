07afe9d17483ec2c49e683010b80e48a326565ee
Kbenaka <kbenaka1407@gmail.com>
2025-03-26 12:47:42 +0530

    sample module with views

---
+++ sample_module/__manifest__.py
@@ -0,0 +1,22 @@
+{
+	'name': 'Sample Module',
+	'version': '18.0.0.0',
+	'summary': 'This Module for training purpose',
+	'description': """This Module for training purpose.
+	""",
+	'category':'',
+	'author': 'benaka k',
+	'website': 'www.zbeanztech.com',
+	"license": "LGPL-3",
+	'depends': [],
+	'data': [   'security/ir.model.access.csv',
+		        'views/model_one_view.xml',
+		        'views/menu.xml',
+		],
+	'test': [],
+	'demo': [],
+	'installable': True,
+	'autoinstall': False,
+	'application': False,
+
+}
